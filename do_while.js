1d949f6748851ddbfcc6c86862837a9fefb79d91
OhYounjae <younjae@kookmin.ac.kr>
2018-05-13 06:44:19 +0900

    20180510

---
+++ do_while.js
@@ -0,0 +1,11 @@
+var answer = parseInt(prompt("5 * 7 = "));
+while(answer != 35){
+  answer = parseInt(prompt("5 * 7"));
+}
+console.log("You got a correct answer");
+
+do{
+  answer = parseInt(prompt("5 * 7 = "));
+}while(answer != 35)
+
+console.log("You got a correct answer with do-while");
