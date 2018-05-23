0d8ae789c16ce1eea690718eca1cafd614bb2888
OhYounjae <younjae@kookmin.ac.kr>
2018-05-24 08:41:34 +0900

    20180524

--- add_new_element.js
+++ add_new_element.js
@@ -3,4 +3,15 @@ var content = document.createTextNode("new strings");
 p_elem.appendChild(content);
 
 var target = document.getElementById("first-p");
-target.appendChild(p_elem);
+
+function add(){
+  target.appendChild(p_elem);
+}
+
+function rm(){
+  target.removeChild(p_elem);
+}
+
+//after covering defer
+var add_id = setInterval(add, 3000);
+var rm_id = setInterval(rm, 7000);
