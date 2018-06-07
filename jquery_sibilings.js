a612bd70e400d61094a1256225d4f272329a1b94
OhYounjae <younjae@kookmin.ac.kr>
2018-06-07 15:39:23 +0900

    20180605

---
+++ jquery_sibilings.js
@@ -0,0 +1,7 @@
+$('#target').prev().attr('style', 'color:red');
+$('#target').prevAll().attr('style', 'color:red');
+$('#target').prevUntil().attr('style', 'color:blue');
+
+$('#target').next().attr('style', 'color:green');
+$('#target').nextAll().attr('style', 'color:green');
+$('#target').nextUntil().attr('style', 'color:red');
