56833413137a05c7018549dad729aed4be6909b6
OhYounjae <younjae@kookmin.ac.kr>
2018-06-12 17:38:38 +0900

    20180612

---
+++ jquery_fading.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#animation-button").click(function(){
+    $("#demo-1").fadeToggle(5000);
+    $("#demo-2").fadeToggle("slow");
+  });
+});
---
+++ jquery_hide_show.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#show").click(function(){
+    $("#demo-1").show(2000);
+  });
+
+  $("#hide").clicke(function(){
+    $("#demo-1").hide();
+  });
+});
